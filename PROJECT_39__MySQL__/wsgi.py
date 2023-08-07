ea11a4bef9f8b2116dced51acd7c0f00c2dd8fa9
Tushar-Kumar-9090 <kumar.tusharpanda@gmail.com>
2023-08-08 00:44:09 +0530

    first commit

---
+++ PROJECT_39__MySQL__/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PROJECT_39__MySQL__ project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJECT_39__MySQL__.settings')
+
+application = get_wsgi_application()
